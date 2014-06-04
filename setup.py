95749827d225db59fc7481fdcab89478925ee6c7
Christopher Armstrong <radix@twistedmatrix.com>
2014-06-03 19:01:29 -0500

    update setup.py version to 0.1a3 so it's obvious this isn't 0.1a2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name="effect",
-    version="0.1a2",
+    version="0.1a3",
     description="pure effects for Python",
     long_description="A way to isolate effects (IO and state manipulation) "
                      "from the rest of your code",
