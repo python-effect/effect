208d9db7c49861a21dd24baf98244d722ec95d50
Christopher Armstrong <radix@twistedmatrix.com>
2014-12-21 13:10:26 -0600

    a bit of a glossary in the __init__ docstring

--- effect/__init__.py
+++ effect/__init__.py
@@ -9,6 +9,18 @@ TODO: an effect implementation of ParallelEffects that is actually serial,
       as a fallback?
 TODO: integration with other asynchronous libraries like asyncio, trollius,
       eventlet
+
+Tour:
+
+- :obj:`Effect`: An object which wraps an Intent and specifies callbacks to be
+  run.
+- Intent: An object which specifies some action to perform.
+- Dispatcher: A callable that takes an Intent and finds the Performer that can
+  execute it (or None).
+- Performer: A callable that takes the dispatcher and a Box. It executes the
+  Intent and puts the result in the Box.
+- Box: An object that has 'succeed' and 'fail' methods for providing the result
+  of an execution (potentially asynchronously).
 """
 
 from __future__ import print_function
