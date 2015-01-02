450ab557a3c80c07b28a00013cedd821b7e60283
Christopher Armstrong <radix@twistedmatrix.com>
2015-01-01 18:34:28 -0600

    fix up docs a bit

---
+++ examples/sync_main.py
@@ -0,0 +1,59 @@
+"""
+Run this example with:
+    python -m examples.github.sync_main
+
+This is an example of using Effect in a normal program that uses
+synchronous/blocking functions to do I/O.
+
+This code has these responsibilities:
+
+- set up a dispatcher that knows how to find performers for all intents
+  used in this application. The application uses ReadLine, HTTPRequest, and
+  ParallelEffects.
+- use :func:`effect.sync_perform` to perform an effect, which returns the
+  result of the effect (or raises an exception it it failed).
+"""
+
+
+from functools import partial
+from multiprocessing.pool import ThreadPool
+
+from effect import (
+    ComposedDispatcher,
+    ParallelEffects,
+    TypeDispatcher,
+    perform_parallel_with_pool,
+    sync_perform)
+
+from .github_example import main_effect
+from .http_intent import HTTPRequest
+from .readline_intent import ReadLine, perform_readline_stdin
+from .sync_http import perform_request_requests
+
+
+def get_dispatcher():
+    """
+    Create a dispatcher that can find performers for :obj:`ReadLine`,
+    :obj:`HTTPRequest`, and :obj:`ParallelEffects`.  There's a built-in
+    performer for ParallelEffects that uses a multiprocessing ThreadPool,
+    :func:`effect.perform_parallel_with_pool`.
+    """
+    my_pool = ThreadPool()
+    pool_performer = partial(perform_parallel_with_pool, my_pool)
+    return ComposedDispatcher([
+        TypeDispatcher({
+            ReadLine: perform_readline_stdin,
+            HTTPRequest: perform_request_requests,
+            ParallelEffects: pool_performer,
+        })
+    ])
+
+
+def main():
+    dispatcher = get_dispatcher()
+    eff = main_effect()
+    print sync_perform(dispatcher, eff)
+
+
+if __name__ == '__main__':
+    main()
---
+++ examples/twisted_main.py
@@ -0,0 +1,58 @@
+"""
+Run this example with:
+    python -m examples.github.twisted_main
+
+This is an example of using Effect with Twisted.
+
+It's important to note that none of the application code relies on Twisted --
+this is the only file that has any dependencies on Twisted. There's also an
+example of running the same application code in ``sync_main.py`` in the same
+directory.
+
+This code has these responsibilities:
+
+- set up a dispatcher that knows how to find performers for all intents
+  used in this application. The application uses ReadLine, HTTPRequest, and
+  ParallelEffects.
+- use :func:`effect.twisted.perform` to perform an effect, which returns a
+  Deferred that we can return from our react function.
+"""
+
+from __future__ import print_function
+
+from twisted.internet.task import react
+
+from effect.twisted import make_twisted_dispatcher, perform
+from effect import (
+    ComposedDispatcher,
+    TypeDispatcher)
+
+from .http_intent import HTTPRequest
+from .twisted_http import perform_request_with_treq
+from .github_example import main_effect
+from .readline_intent import ReadLine, perform_readline_stdin
+
+
+def get_dispatcher(reactor):
+    """
+    Create a dispatcher that can find performers for :obj:`ReadLine`,
+    :obj:`HTTPRequest`, and :obj:`ParallelEffects`.
+    :func:`make_twisted_dispatcher` is able to provide the ``ParallelEffects``
+    performer, so we compose it with our own custom :obj:`TypeDispatcher`.
+    """
+    return ComposedDispatcher([
+        TypeDispatcher({
+            ReadLine: perform_readline_stdin,
+            HTTPRequest: perform_request_with_treq,
+        }),
+        make_twisted_dispatcher(reactor),
+    ])
+
+
+def main(reactor):
+    dispatcher = get_dispatcher(reactor)
+    eff = main_effect()
+    return perform(dispatcher, eff).addCallback(print)
+
+if __name__ == '__main__':
+    react(main, [])
