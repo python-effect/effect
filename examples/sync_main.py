d51b18c5dc90d165b7be62d3946857faed2ef224
Christopher Armstrong <radix@twistedmatrix.com>
2015-01-01 18:30:22 -0600

    docs for sync_main.py

--- examples/sync_main.py
+++ examples/sync_main.py
@@ -1,3 +1,20 @@
+"""
+Run this example with:
+    python -m examples.sync_main
+
+This is an example of using Effect in a normal program that uses
+synchronous/blocking functions to do I/O.
+
+This code has these responsibilities:
+
+- set up a dispatcher that knows how to find performers for all intents
+  used in this application. The application uses ReadLine, HTTPRequest, and
+  ParallelEffects.
+- use :func:`effect.sync_perform` to perform an effect, which returns the
+  result of the effect (or raises an exception it it failed).
+"""
+
+
 from functools import partial
 from multiprocessing.pool import ThreadPool
 
@@ -15,6 +32,12 @@ from .sync_http import perform_request_requests
 
 
 def get_dispatcher():
+    """
+    Create a dispatcher that can find performers for :obj:`ReadLine`,
+    :obj:`HTTPRequest`, and :obj:`ParallelEffects`.  There's a built-in
+    performer for ParallelEffects that uses a multiprocessing ThreadPool,
+    :func:`effect.perform_parallel_with_pool`.
+    """
     my_pool = ThreadPool()
     pool_performer = partial(perform_parallel_with_pool, my_pool)
     return ComposedDispatcher([
