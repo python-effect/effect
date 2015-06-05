7c934c1f02f4650de4c569183ccb4ae97f4c1d05
Christopher Armstrong <radix@twistedmatrix.com>
2015-06-04 22:34:11 -0500

    support `return` in generators for Py3-only users.

--- effect/do.py
+++ effect/do.py
@@ -32,6 +32,8 @@ def do(f):
     yielded Effect will be passed back into the generator as the result of the
     ``yield`` expression. Yielded :func:`do_return` values will provide the
     ultimate result of the Effect that is returned by the decorated function.
+    Note that :func:`do_return` is only necessary for Python 2 compatibility;
+    return statements can be used directly in Python 3-only code.
 
     It's important to note that any generator function decorated by ``@do``
     will no longer return a generator, but instead it will return an Effect,
@@ -78,6 +80,9 @@ def do_return(val):
         @do
         def foo():
             yield do_return('hello')
+
+    If you're writing Python 3-only code, you don't need to use this function,
+    and can just use the `return` statement as normal.
     """
     return _ReturnSentinel(val)
 
@@ -88,7 +93,7 @@ def _do(result, generator, is_error):
             val = generator.throw(*result)
         else:
             val = generator.send(result)
-    except StopIteration:
+    except StopIteration as stop:
         # If the generator we're spinning directly raises StopIteration, we'll
         # treat it like returning None from the function. But there may be a
         # case where some other code is raising StopIteration up through this
@@ -98,7 +103,12 @@ def _do(result, generator, is_error):
         if tb.tb_next:
             raise
         else:
-            return None
+            # Python 3 allows you to use `return val` in a generator, which
+            # will be translated to a `StopIteration` with a `value` attribute
+            # set to the return value. So we'll return that value as the
+            # ultimate result of the effect. Python 2 doesn't have the 'value'
+            # attribute of StopIteration, so we'll fall back to None.
+            return getattr(stop, 'value', None)
     if type(val) is _ReturnSentinel:
         return val.result
     elif type(val) is Effect:
