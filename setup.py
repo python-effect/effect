b3c74a441463713dd117c8501e4e1c74c93d5c29
Christopher Armstrong <radix@twistedmatrix.com>
2015-06-07 23:57:43 -0500

    prepare for 0.9 (and drastically simplify docs/source/conf.py)

--- setup.py
+++ setup.py
@@ -3,14 +3,13 @@ import setuptools
 
 setuptools.setup(
     name="effect",
-    version="0.1a19",
+    version="0.9",
     description="pure effects for Python",
     long_description=open('README.rst').read(),
     url="http://github.com/python-effect/effect/",
     author="Christopher Armstrong",
     license="MIT",
     classifiers=[
-        'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 3',
