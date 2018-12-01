c739312d34c5740eb0945c487a0a5e0429cf6359
Christopher Armstrong <227068+radix@users.noreply.github.com>
2018-12-01 11:38:24 -0600

    bump version to 0.12.0+

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,6 +12,6 @@ source_suffix = '.rst'
 master_doc = 'index'
 project = u'Effect'
 copyright = u'2015, Christopher Armstrong'
-version = release = '0.12.0'
+version = release = '0.12.0+'
 
 html_theme = 'sphinx_rtd_theme'
